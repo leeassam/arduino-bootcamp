7d9db358b3973e6bc6a8df593b5e78b11e0f885f
Lee Assam <leeassam@gmail.com>
2016-12-21 14:30:04 -0600

    modified dual servo sweep

--- Wii_Pan_Tilt/DualServoSweep/DualServoSweep.ino
+++ Wii_Pan_Tilt/DualServoSweep/DualServoSweep.ino
@@ -34,7 +34,7 @@ void loop() {
   }
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo1.write(pos);              // tell servo to go to position in variable 'pos'
-    myservo1.write(pos); 
+    myservo2.write(pos); 
     delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
